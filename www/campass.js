f5d2a7f9afaae8d12247372e85a6dc8e18566e07
helicc16 <kiennd53@gmail.com>
2016-08-09 16:48:27 -0400

    Update campass.js

--- www/campass.js
+++ www/campass.js
@@ -1,7 +1,22 @@
 module.exports = {
     setAzimuth: function (successCallback) {
-        cordova.exec(successCallback,
-        null, // skipping error callback
+
+
+        var win = function (result) {
+
+            alert('Hello');
+            var resultString = result;
+            successCallback(resultString);
+        };
+
+        var fail = function () {
+            alert('Fail to return result');
+            console.log('Fail to return result');
+        };
+
+
+        cordova.exec(win,
+        fail, // skipping error callback
         "Campass",
         "setAzimuth",
         []);
